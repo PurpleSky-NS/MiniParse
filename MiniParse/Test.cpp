feb4e6bbb8ae8afda9bd899a1367e4384459cd92
root <root@localhost>
2019-09-19 09:52:12 +0800

    Array的完善与DynamicArray的补充

--- MiniParse/Test.cpp
+++ MiniParse/Test.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Various.h"
 #include "DynamicSizeArray.h"
 #include "FixedSizeArray.h"
@@ -12,6 +12,7 @@ int main()
 	table.UpdateVarious("测试静态数组(1)", FixedSizeArray::GetFixedSizeArray(5));
 	table.UpdateVarious("测试静态数组(2)", FixedSizeArray::GetFixedSizeArray({ 1,2,3.3,5.4 }));
 	table.UpdateVarious("测试静态数组(3)", FixedSizeArray::GetFixedSizeArray(6, { 1,2,3,5 }));
+	table.UpdateVarious("测试动态数组", DynamicSizeArray::GetDynamicSizeArray({ 1,2,3,5 }));
 
 	FixedSizeArray &f = *((FixedSizeArray*)table.GetVarious("测试静态数组(1)"));
 	FixedSizeArray &ff = *((FixedSizeArray*)table.GetVarious("测试静态数组(2)"));
@@ -20,6 +21,17 @@ int main()
 	cout << "测试静态数组(1) : " << f.Size() << "->[3]=" << f[3].value << endl;
 	cout << "测试静态数组(2) : " << ff.Size() << "->[3]=" << ff[3].value << endl;
 	cout << "测试静态数组(3) : " << fff.Size() << "->[3]=" << fff[3].value << endl;
+	
+	DynamicSizeArray &d = *((DynamicSizeArray*)table.GetVarious("测试动态数组"));
+	cout << "测试动态数组 : " << d.Size() << "->[1]=" << d[1].value << endl;
+	d.Add(12.3);
+	cout << "测试动态数组 : " << d.Size() << "->[4]=" << d[4].value << endl;
+	d.RemoveLast();
+	d.Remove(0);
+	cout << "测试动态数组 : " << d.Size() << "->[0]=" << d[0].value << endl;
+	cout << "测试动态数组 : " << d.Size() << "->[?]=" << d.Contains(2) << endl;
+	
+	
 	cin.get();
 	return 0;
 }
