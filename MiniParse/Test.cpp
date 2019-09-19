756cceccec9e320cc6f27f1f03cb1164b56622cb
root <root@localhost>
2019-09-19 11:12:14 +0800

    测试

--- MiniParse/Test.cpp
+++ MiniParse/Test.cpp
@@ -5,33 +5,23 @@
 #include "VariousTable.h"
 
 using namespace std;
+
+bool Test(function<DynamicSizeArray *(const DynamicSizeArray &)> func,const DynamicSizeArray &testIn,const DynamicSizeArray &testOut)
+{
+	func(testIn)->Foreach([](Array::ArrayItem& i){cout<<i.value<<endl;});
+	return *func(testIn)==testOut;
+}
 int main()
 {
-	VariousTable table;
-	table.UpdateVarious("测试变量", Various::GetVarious(520));
-	table.UpdateVarious("测试静态数组(1)", FixedSizeArray::GetFixedSizeArray(5));
-	table.UpdateVarious("测试静态数组(2)", FixedSizeArray::GetFixedSizeArray({ 1,2,3.3,5.4 }));
-	table.UpdateVarious("测试静态数组(3)", FixedSizeArray::GetFixedSizeArray(6, { 1,2,3,5 }));
-	table.UpdateVarious("测试动态数组", DynamicSizeArray::GetDynamicSizeArray({ 1,2,3,5 }));
-
-	FixedSizeArray &f = *((FixedSizeArray*)table.GetVarious("测试静态数组(1)"));
-	FixedSizeArray &ff = *((FixedSizeArray*)table.GetVarious("测试静态数组(2)"));
-	FixedSizeArray &fff = *((FixedSizeArray*)table.GetVarious("测试静态数组(3)"));
-	cout << "测试变量 : " << ((Various*)table.GetVarious("测试变量"))->GetValue() << endl;
-	cout << "测试静态数组(1) : " << f.Size() << "->[3]=" << f[3].value << endl;
-	cout << "测试静态数组(2) : " << ff.Size() << "->[3]=" << ff[3].value << endl;
-	cout << "测试静态数组(3) : " << fff.Size() << "->[3]=" << fff[3].value << endl;
+	auto t0=[](const DynamicSizeArray &in)->DynamicSizeArray*
+	{
+		DynamicSizeArray &d=*DynamicSizeArray::GetDynamicSizeArray(in);
+		d.RemoveIf([](const Array::ArrayItem &item){return item.value==5;});
+		d.RemoveRangeIf([](const Array::ArrayItem &item){return item.value==2;});
+		return &d;
+	};
 	
-	DynamicSizeArray &d = *((DynamicSizeArray*)table.GetVarious("测试动态数组"));
-	cout << "测试动态数组 : " << d.Size() << "->[1]=" << d[1].value << endl;
-	d.Add(12.3);
-	cout << "测试动态数组 : " << d.Size() << "->[4]=" << d[4].value << endl;
-	d.RemoveLast();
-	d.Remove(0);
-	cout << "测试动态数组 : " << d.Size() << "->[0]=" << d[0].value << endl;
-	cout << "测试动态数组 : " << d.Size() << "->[?]=" << d.Contains(2) << endl;
-	
-	
+	cout<<Test(t0,*DynamicSizeArray::GetDynamicSizeArray({1,5,5,5,2,2,2}),*DynamicSizeArray::GetDynamicSizeArray({1,5,5}))<<endl;
 	cin.get();
 	return 0;
 }
