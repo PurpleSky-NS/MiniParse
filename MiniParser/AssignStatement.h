139220fbd17479c638a051e2316a4c67610852a0
root <root@localhost>
2019-09-20 10:20:19 +0800

    构造函数

--- MiniParser/AssignStatement.h
+++ MiniParser/AssignStatement.h
@@ -9,6 +9,12 @@ class AssignStatement :public Statement
 {
 public:
 
+	inline AssignStatement(const std::string &variousName,PostfixExpression *expression);
+	inline AssignStatement(const std::string &arrayName,unsigned pos,PostfixExpression *expression);
+	AssignStatement(const AssignStatement&)=delete;
+	AssignStatement(AssignStatement&&)=delete;
+	~AssignStatement();
+	
 	inline virtual bool Excuse() override;
 
 	virtual void Save(std::ostream &out) override;
@@ -19,29 +25,36 @@ public:
 
 private:
 	
-	friend class Parser;
-	
 	VariousBase::VariousType m_LType;
 	union
 	{
 		struct
 		{
-			std::string m_arrayName;
-			unsigned m_arrayPos;
+			std::string arrayName;
+			unsigned arrayPos;
 		}arrayValue;
 		struct
 		{
-			std::string m_variousName;
+			std::string variousName;
 		}variousValue;
 	}m_value;
 	PostfixExpression *m_expression;
-	
-	inline AssignStatement(const std::string &variousName,PostfixExpression *expression);
-	inline AssignStatement(const std::string &arrayName,unsigned pos,PostfixExpression *expression);
-	AssignStatement(const AssignStatement&)=delete;
-	AssignStatement(AssignStatement&&)=delete;
-	~AssignStatement();
 };
+
+AssignStatement::AssignStatement(const std::string &variousName,PostfixExpression *expression)
+{
+	m_LType=VariousBase::Various;
+	m_value.variousValue.variousName=variousName;
+	m_expression=expression;
+}
+
+AssignStatement::AssignStatement(const std::string &arrayName,unsigned pos,PostfixExpression *expression)
+{
+	m_LType=VariousBase::Array;
+	m_value.arrayValue.arrayName=arrayName;
+	m_value.arrayValue.arrayPos=pos;
+	m_expression=expression;
+}
 
 bool AssignStatement::Excuse()
 {
