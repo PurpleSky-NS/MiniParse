12a37f89a73bd670f1ff0e27286eacad56c8edc8
PurpleSky-NS <wpq2225232027@163.com>
2019-09-21 20:19:15 +0800

    更改编码

--- MiniParser/IFreeable.h
+++ MiniParser/IFreeable.h
@@ -1,9 +1,9 @@
-#pragma once
+﻿#pragma once
 class IFreeable
 {
 public:
-	/*��һ�������delete����������ʹ��Free�ӿڱ����ö��󲻻���ʹ��
-	Ӧ��ȷ�����øýӿڵ�������Ƕ�̬�����*/
+	/*不一定会进行delete操作，但会使用Free接口表明该对象不会再使用
+	应该确保调用该接口的类对象都是动态分配的*/
 	virtual void Free() = 0;
 };
 
