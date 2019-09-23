caa218c93b0adddb67da9b76947dbd8530d0e96d
root <root@localhost>
2019-09-23 11:58:09 +0800

    括号

---
+++ MiniParser/BracketItem.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include "ItemBase.h"
+
+class BracketItem : public ItemBase
+{
+public:
+
+	enum BracketType : char
+	{
+		Left = 0,		//(
+		Right,		//)
+		Count
+	};
+
+	static BracketItem* GetBracket(BracketType type);
+
+	inline void SetBracketType(BracketType type);
+
+	inline BracketType GetBracketType()const;
+
+	virtual inline ItemType GetType() const override;
+
+	virtual inline void Save(std::ostream& out) override;
+
+	virtual void Free()override {}
+
+private:
+
+	static BracketItem brackets[Count];
+
+	inline BracketItem() = default;
+	inline BracketItem(BracketType type);
+	inline BracketItem(const BracketItem&) = default;
+	inline BracketItem(BracketItem&&) = default;
+	inline ~BracketItem() = default;
+
+	BracketType m_type;
+};
+BracketItem::BracketItem(BracketType type)
+{
+	m_type = type;
+}
+
+void BracketItem::SetBracketType(BracketType type)
+{
+	m_type = type;
+}
+
+BracketItem::BracketType BracketItem::GetBracketType() const
+{
+	return m_type;
+}
+
+BracketItem::ItemType BracketItem::GetType() const
+{
+	return ItemBase::Bracket;
+}
+
+void BracketItem::Save(std::ostream& out)
+{
+	out.put(ItemBase::Bracket);
+	out.put(m_type);
+}
