1dc9942a622bf0c0f2af3a35c4cdcefa337c0249
root <root@localhost>
2019-09-23 11:29:50 +0800

    构造函数

--- MiniParser/Expression.h
+++ MiniParser/Expression.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <vector>
 #include "IFreeable.h"
@@ -7,12 +7,20 @@
 class Expression :public IFreeable, public ISavable
 {
 public:
+	
+	inline Expression()=default;
+	inline Expression(const Expression &)=delete;
+	inline Expression(Expression&&)=delete;
+	inline ~Expression();
 
 	/*获取表达式元素序列*/
 	inline const std::vector<ItemBase*>& GetExpression()const;
 
 	/*添加元素*/
 	inline void AddItem(ItemBase *item);
+	
+	/*清空表达式*/
+	inline void Clear();
 
 	inline virtual void Free();
 
@@ -30,9 +38,19 @@ void Expression::AddItem(ItemBase *item)
 	m_expression.push_back(item);
 }
 
-void Expression::Free()
+void Expression::Clear()
 {
 	for (auto i : m_expression)
 		i->Free();
+	m_expression.clear();
+}
+
+void Expression::Free()
+{
 	delete this;
+}
+
+Expression::~Expression()
+{
+	Clear();
 }
