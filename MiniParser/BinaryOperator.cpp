f100617b9125ae1f3950c6c9869f2b0ed50f761e
root <root@localhost>
2019-09-25 00:17:07 +0800

    加了乘方

--- MiniParser/BinaryOperator.cpp
+++ MiniParser/BinaryOperator.cpp
@@ -1,11 +1,12 @@
-﻿#include "BinaryOperator.h"
+#include "BinaryOperator.h"
 BinaryOperator BinaryOperator::operators[Count]
 {
 	BinaryOperator((BinaryOperator::BinaryOperatorType)0),
 	BinaryOperator((BinaryOperator::BinaryOperatorType)1),
 	BinaryOperator((BinaryOperator::BinaryOperatorType)2),
 	BinaryOperator((BinaryOperator::BinaryOperatorType)3),
-	BinaryOperator((BinaryOperator::BinaryOperatorType)4)
+	BinaryOperator((BinaryOperator::BinaryOperatorType)4),
+	BinaryOperator((BinaryOperator::BinaryOperatorType)5)
 };
 BinaryOperator* BinaryOperator::GetBinaryOperator(BinaryOperatorType type)
 {
