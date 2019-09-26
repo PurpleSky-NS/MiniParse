a114f8c878013d714abedb2196075d349c5f8c8e
root <root@localhost>
2019-09-26 12:10:41 +0800

    新增

---
+++ MiniParser/VariousIDF.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "IdentificationItem.h"
+
+/*作为变量的标识符，仅作为标识符，应在表达式中将之替换为数值*/
+class VariousIDF : public IdentificationItem
+{
+public:
+
+	VariousIDF() = delete;
+	inline VariousIDF(const std::string &name);
+	inline VariousIDF(const VariousIDF&) = delete;
+	inline VariousIDF(VariousIDF&&) = delete;
+	inline ~VariousIDF() = default;
+	
+	inline virtual IdentificationType GetIdentificationType()const;
+
+	inline virtual void Save(std::ostream& out) override;
+
+	inline virtual void Free() override;
+	
+};
+
+VariousIDF::VariousIDF(const std::string &name):
+    IdentificationItem(name)
+{}
+
+VariousIDF::IdentificationType VariousIDF::GetIdentificationType() const
+{
+	return IdentificationType::VariousIDF;
+}
+
+void VariousIDF::Save(std::ostream& out)
+{
+	out.put(IdentificationType::VariousIDF);
+}
+
+void VariousIDF::Free()
+{
+	delete this;
+}
